3a687536e66e774609853c9bfcaf4f7b1484325d
alialyon <alialyon123@gmail.com>
2024-12-30 13:50:32 +0100

    Ajout de la nouvelle fonctionnalité

---
+++ src/app/nouvelle-fonctionnalite/nouvelle-fonctionnalite.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nouvelle-fonctionnalite',
+  standalone: true,
+  imports: [],
+  templateUrl: './nouvelle-fonctionnalite.component.html',
+  styleUrl: './nouvelle-fonctionnalite.component.css'
+})
+export class NouvelleFonctionnaliteComponent {
+
+}
